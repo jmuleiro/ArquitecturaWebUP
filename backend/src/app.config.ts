e7db47d18b3ca79f75463b29d3fb8240b76d5480
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2026-05-16 22:37:47 -0300

    using nestjs/swagger to build the REST API

--- backend/src/app.config.ts
+++ backend/src/app.config.ts
@@ -4,7 +4,8 @@ export default () => ({
   app: {
     name: "AWTP",
     port: process.env.port ?? 3000,
-    logLevel: process.env.LOG_LEVEL ?? LOG_LEVELS[2] // todo: validate
+    logLevel: process.env.LOG_LEVEL ?? LOG_LEVELS[2], // todo: validate
+    env: process.env.NODE_ENV ?? "production",
   },
   database: {
     host: process.env.DATABASE_HOST || "localhost",
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,35 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   const configService = app.get(ConfigService);
+
   app.useLogger(new ConsoleLogger({
     prefix: configService.get("app.name"),
     logLevels: [configService.get("app.logLevel")],
     timestamp: false
-  }))
+  }));
+
+  const docBuilder = new DocumentBuilder()
+    .setTitle("Stock Management")
+    .setDescription("Stock & Category Management API")
+    .setVersion('1.0')
+    //    .addTag('')
+    .build();
+
+  const options: SwaggerCustomOptions = {
+    ui: configService.get("app.env") != "production",
+    raw: configService.get("app.env") != "production" ? ['json'] : [],
+    explorer: configService.get("app.env") != "production",
+  }
+
+  const documentFactory = () => SwaggerModule.createDocument(app, docBuilder);
+  SwaggerModule.setup('api', app, documentFactory, options);
+
   await app.listen(configService.get("app.port")!);
 }
 bootstrap();
