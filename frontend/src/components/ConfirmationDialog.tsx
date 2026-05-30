b3580b8f790446ff9bb781a4dc46e22bed3d40ca
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2026-05-30 11:19:45 -0300

    confirmation dialog component

---
+++ frontend/src/components/ConfirmationDialog.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Button, Dialog, DialogTitle, DialogActions } from "@mui/material";
+
+export interface ConfirmationDialogProps {
+  open: boolean;
+  onClose: (value: string) => void;
+  message: string;
+}
+
+export default function ConfirmationDialog(props: ConfirmationDialogProps) {
+  // const [open, setOpen] = useState(false); // This goes outside
+  const { onClose, open, message } = props;
+
+  return (
+    <Dialog onClose={onClose} open={open}>
+      <DialogTitle>{message}</DialogTitle>
+      <DialogActions>
+        <Button variant="contained" onClick={() => onClose('confirm')}>Confirm</Button>
+        <Button variant="outlined" onClick={() => onClose('cancel')}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
